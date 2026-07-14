a30a52d836aef24aa77df27155bbe0526b657470
Vinicius.Silva <vinicius.silva@concreto.com.br>
2026-07-14 17:14:34 -0300

    Update dependencias

--- src/categories/dto/update-category.dto.ts
+++ src/categories/dto/update-category.dto.ts
@@ -1,7 +1,7 @@
 import { IsOptional, IsString, MaxLength } from 'class-validator';
 
 export class UpdateCategoryDto {
-  @IsOptional()
+  @IsOptional() // Esse campo pode nao vir na requisicao
   @IsString()
   @MaxLength(50)
   name?: string; // O ? torna a propriedade opcional em typescript
